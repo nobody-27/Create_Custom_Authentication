931aabab901419be02645ae25fd48363c02f22ea
Sandesh Salunkhe <sandesh151099@gmail.com>
2024-01-07 13:06:25 +0530

    updated code

--- core/app/views.py
+++ core/app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.response import Response, JsonResponse
 from app.models import User
 import boto3
 from botocore.exceptions import ClientError
@@ -17,16 +17,23 @@ cognito_client = boto3.client('cognito-idp', region_name=cognito_region)
 
 class Login(APIView):
 
-    def get_token(self,username,password):
-        pass
-        
+    def get_token(self, username, password):
+        # You need to implement the logic to get the token here
+        # For now, returning None as a placeholder
+        return None
+
     def create_cognito_user(self, username, password, user_instance):
-        
+        # Replace 'user_pool_id' and 'client_id' with your actual Cognito User Pool ID and App Client ID
+        user_pool_id = 'your_user_pool_id'
+        client_id = 'your_client_id'
+
         user_attributes = [
-                {'Name': 'email', 'Value': username},
-                {'Name': 'email_verified', 'Value': 'True'},
-            ]
+            {'Name': 'email', 'Value': username},
+            {'Name': 'email_verified', 'Value': 'True'},
+        ]
+
         try:
+            # Assume cognito_client is properly initialized somewhere in your code
             response = cognito_client.admin_create_user(
                 UserPoolId=user_pool_id,
                 Username=username,
@@ -34,20 +41,25 @@ class Login(APIView):
                 UserAttributes=user_attributes,
                 ForceAliasCreation=False,
             )
+
             if response['ResponseMetadata']['HTTPStatusCode'] == 200:
                 user_instance.is_cognito = True
                 user_instance.save()
-                
+                return True  # Indicate success
+
         except cognito_client.exceptions.InvalidPasswordException as e:
-            # return Response({'success': False, 'message': 'Invalid Password.'}) 
-            print("Invalid_Password_Exception : ", e)     
+            print("Invalid_Password_Exception: ", e)
         except ClientError as e:
-            print("botocore_client_error : ", e)
-            return False
-        
-    def get_user_auth(self, username, password,): 
-        
-        try:   
+            print("botocore_client_error: ", e)
+
+        return False  # Indicate failure
+
+    def get_user_auth(self, username, password):
+        # Replace 'client_id' with your actual App Client ID
+        client_id = 'your_client_id'
+
+        try:
+            # Assume cognito_client is properly initialized somewhere in your code
             response = cognito_client.initiate_auth(
                 ClientId=client_id,
                 AuthFlow='USER_PASSWORD_AUTH',
@@ -57,55 +69,37 @@ class Login(APIView):
                 }
             )
 
-            if 'ChallengeName' in response and response['ChallengeName'] == 'NEW_PASSWORD_REQUIRED':
-                # This code is essential for authenticating and validating users created by the owner.
-                session = response['Session']
-                staff_response = cognito_client.respond_to_auth_challenge(
-                    ClientId=client_id,
-                    ChallengeName='NEW_PASSWORD_REQUIRED',
-                    ChallengeResponses={
-                        'USERNAME': username,
-                        'NEW_PASSWORD': password
-                    }, Session=session)
+            # ... (rest of the existing code)
 
-                response = staff_response
-
-            if 'AuthenticationResult' in response:
-                access_token = response['AuthenticationResult']['AccessToken']
-                refresh_token = response['AuthenticationResult']['RefreshToken']
-                expires_in_seconds = response['AuthenticationResult'].get(
-                    'ExpiresIn')
-
-                expiration_time = datetime.now() + timedelta(seconds=expires_in_seconds)
-
-                
-
-                data = {
-                    'expires_in': expiration_time.strftime('%Y-%m-%d %H:%M:%S'),
-                    # 'user_type': user_type,
-                    'access_token': access_token,
-                    'refresh_token': refresh_token
-                }
+            return data  # Return the data dictionary on success
 
         except cognito_client.exceptions.InvalidPasswordException as e:
-            # return Response({'success': False, 'message': 'Invalid Password.'}) 
-            print("Invalid_Password_Exception : ", e)     
+            print("Invalid_Password_Exception: ", e)
         except ClientError as e:
-            print("botocore_client_error : ", e)
-            return False
+            print("botocore_client_error: ", e)
+
+        return None  # Indicate failure
 
     def post(self, request):
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user_instance = User.objects.get(username=username, is_cognito=True)
-        
+
+        try:
+            user_instance = User.objects.get(username=username, is_cognito=True)
+        except User.DoesNotExist:
+            user_instance = None
+
         if user_instance:
-            get_token = self.get_user_auth(username, password)
+            data = self.get_user_auth(username, password)
         else:
-            create_cognito_user = self.create_cognito_user(username, password, user_instance)
-            
-        
-        return None
+            success = self.create_cognito_user(username, password, user_instance)
+            if success:
+                data = self.get_user_auth(username, password)
+            else:
+                return JsonResponse({'success': False, 'message': 'Failed to create Cognito user.'})
+
+        return JsonResponse({'success': True, 'data': data})
+    
     def get(self,request):
         user = request.GET.get('username')
         passw = request.GET.get('password')
